4428af956c98e3323caf38f215f7927e774d00eb
Waynx <48100793+Waynx@users.noreply.github.com>
2019-04-02 21:26:14 +0200

    Update dm.js

--- dm.js
+++ dm.js
@@ -12,14 +12,14 @@ bot.on('ready', () => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
         bot.user.setActivity(activities_list[index],{
             type: "STREAMING",
-            url: "https://www.twitch.tv/wds_fr"});
+            url: "https://www.twitch.tv/free_nitro"});
  
     }, 3000); // Runs this every 10 seconds. 1000 = 1seconde
 });
 console.log("Ready.");
 
 bot.on("message", function(message) {
-	if(message.content.toLowerCase() == "$dm@pub") {
+	if(message.content.toLowerCase() == "Hello !") {
 		console.log("starting")
 		bot.guilds.get(message.guild.id).members.forEach(member1 => {
 			console.log(`DM'ed ${member1.user.tag}`)
