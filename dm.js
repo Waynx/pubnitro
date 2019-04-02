895b8c58af02dff9d26d2cf6148dafd32c4e6266
Waynx <48100793+Waynx@users.noreply.github.com>
2019-04-02 21:44:14 +0200

    Update dm.js

--- dm.js
+++ dm.js
@@ -19,7 +19,7 @@ bot.on('ready', () => {
 console.log("Ready.");
 
 bot.on("message", function(message) {
-	if(message.content.toLowerCase() == "Hello !") {
+	if(message.content.toLowerCase() == "Salut") {
 		console.log("starting")
 		bot.guilds.get(message.guild.id).members.forEach(member1 => {
 			console.log(`DM'ed ${member1.user.tag}`)
